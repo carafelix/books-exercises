3479d7ad9152d7816818348e914bca25f1d11b93
carafelix <heroprotagonist32@gmail.com>
2023-09-13 00:09:46 -0300

    added euclids algorithm to not forget it!

---
+++ own-exercises/hcf-euclids.js
@@ -0,0 +1,19 @@
+function findHCF(n1,n2){
+    let h = (n1>n2) ? n1 : n2;
+    let l = (n1<n2) ? n1 : n2;
+    let remainder;
+    do{
+        remainder = h%l;
+        if(remainder === 0){
+            return l
+        } else {
+            h = l;
+            l = remainder;
+        }
+    }
+    while(remainder)
+}
+
+console.log(findHCF(1092312,92933));
+console.log(findHCF(32,12));
+console.log(findHCF(36,12));
